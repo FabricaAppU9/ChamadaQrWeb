f52533b8b62452f5dcd2ea068ea9ab10e48d628e
luiz henrique <souza_luiz@live.com>
2018-04-14 08:24:55 -0300

    Adicionando projeto windows

---
+++ ChamadaQrWebAPI/ChamadaQrWebAPI/Models/ChamadaQrContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChamadaQrWebAPI.Models
+{
+    public class ChamadaQrContext : DbContext
+    {
+        public ChamadaQrContext(DbContextOptions<ChamadaQrContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ALUNO> Alunos { get; set; }
+    }
+}
