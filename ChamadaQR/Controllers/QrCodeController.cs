e9f559cc9f63136f96b1597ba9a1665b96ce1bd0
eveoliv <everton_ace@hotmail.com>
2018-09-27 05:34:00 -0300

    Camada de geracao QRCode

--- ChamadaQR/Controllers/QrCodeController.cs
+++ ChamadaQR/Controllers/QrCodeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChamadaQR.Data;
+using ChamadaQR.Data.DAL;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +11,17 @@ namespace ChamadaQR.Controllers
 {
     
     public class QrCodeController : Controller
-    {
+    {       
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Generate(string productId)
+        {
+            ViewBag.productId = productId;
+            return View("Index");
+        }
+
     }
 }
