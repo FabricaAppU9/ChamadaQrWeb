bd362219cf3189cf7ca192474f6896acb431aa52
eveoliv <everton_ace@hotmail.com>
2018-09-27 06:12:08 -0300

    Modulo de testes de geracao do QRCode

---
+++ ChamadaQC/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChamadaQR.Controllers
+{
+    
+    public class DemoController : Controller
+    {       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Generate(string productId)
+        {
+            ViewBag.productId = productId;
+            return View("Index");
+        }
+
+    }
+}
